81968593647f42aae85093d7c4a9283ac8fbee60
Nam Lee <contact@namlee.fr>
2021-01-04 11:03:28 +0100

    factorization gallery

--- app/Views/main/gallery_en.tpl.php
+++ app/Views/main/gallery_en.tpl.php
@@ -1,23 +1,3 @@
 <section class="container">
-
-    <h1>Gallery</h1>
-
-    <div class="d-flex justify-content-between flex-wrap">
-
-        <div class="card border-info my-4" style="width: 20rem;">
-            <img src="<?= $assetsBaseUri ?>images/gallery/BTW.png" class="card-img-top" alt="Between Two Ferns">
-            <div class="card-body text-center">
-                <h5 class="card-title">Between Two Ferns</h5>
-            </div>
-        </div>
-
-        <div class="card border-info my-4" style="width: 20rem;">
-            <img src="<?= $assetsBaseUri ?>images/gallery/VFV.png" class="card-img-top" alt="V For Vendetta">
-            <div class="card-body text-center">
-                <h5 class="card-title">V For Vendetta</h5>
-            </div>
-        </div>
-
-    </div>
-
+    <?php require __DIR__ . '/../partials/pictures.tpl.php' ?>
 </section>
