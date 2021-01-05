169d4312d03ca6daed236dee3ac786666fe05c7a
Nam Lee <contact@namlee.fr>
2021-01-05 18:48:48 +0100

    add info for pictures in the gallery

--- app/Views/partials/pictures.tpl.php
+++ app/Views/partials/pictures.tpl.php
@@ -12,6 +12,10 @@
         <img src="<?= $assetsBaseUri ?>images/gallery/BTW.png" class="card-img-top" alt="Between Two Ferns">
         <div class="card-body text-center">
             <h5 class="card-title">Between Two Ferns</h5>
+            <p>Fil / Yarn : Félicette - <a target="_blank" href="https://lechatquitricote-shop.com/">Le Chat Qui Tricote</a></p>
+            <p>75% merino - 25% nylon - 5% stellina</p>
+            <p>Coloris / Color : Olive</p>
+            <p>Taille / Size : M</p>
         </div>
     </div>
 
@@ -19,6 +23,10 @@
         <img src="<?= $assetsBaseUri ?>images/gallery/VFV.png" class="card-img-top" alt="V For Vendetta">
         <div class="card-body text-center">
             <h5 class="card-title">V For Vendetta</h5>
+            <p>Fil / Yarn : Figaro - <a target="_blank" href="https://lechatquitricote-shop.com/">Le Chat Qui Tricote</a></p>
+            <p>75% merino - 20% yak - 10% nylon</p>
+            <p>Coloris / Color : Paon</p>
+            <p>Taille / Size : M</p>
         </div>
     </div>
 
