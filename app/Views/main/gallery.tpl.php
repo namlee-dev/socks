01ee803c2e1f5893f902f3d2fac2869b62238235
Nam Lee <contact@namlee.fr>
2021-01-06 09:40:43 +0100

    gallery change titles and one page per language

--- app/Views/main/gallery.tpl.php
+++ app/Views/main/gallery.tpl.php
@@ -1,3 +1,30 @@
 <section class="container">
-    <?php require __DIR__ . '/../partials/pictures.tpl.php' ?>
+    <h1>Galerie</h1>
+    <div class="d-flex justify-content-between flex-wrap">
+
+    <div class="card border-info my-4" style="width: 20rem;">
+        <img src="<?= $assetsBaseUri ?>images/gallery/BTW.png" class="card-img-top" alt="Between Two Ferns">
+        <div class="card-body text-center">
+            <h4 class="card-title text-info">Between Two Ferns</h4>
+            <h5>Exemple en :</h5>
+            <p>Fil : Félicette - <a target="_blank" href="https://lechatquitricote-shop.com/">Le Chat Qui Tricote</a></p>
+            <p>75% mérinos - 25% nylon - 5% stellina</p>
+            <p>Coloris : Olive</p>
+            <p>Taille : M</p>
+        </div>
+    </div>
+
+    <div class="card border-info my-4" style="width: 20rem;">
+        <img src="<?= $assetsBaseUri ?>images/gallery/VFV.png" class="card-img-top" alt="V For Vendetta">
+        <div class="card-body text-center">
+            <h4 class="card-title text-info">V For Vendetta</h4>
+            <h5>Exemple en :</h5>
+            <p>Fil : Figaro - <a target="_blank" href="https://lechatquitricote-shop.com/">Le Chat Qui Tricote</a></p>
+            <p>75% mérinos - 20% yak - 10% nylon</p>
+            <p>Coloris : Paon</p>
+            <p>Taille : M</p>
+        </div>
+    </div>
+
+    </div>
 </section>
--- app/Views/main/gallery_en.tpl.php
+++ app/Views/main/gallery_en.tpl.php
@@ -1,3 +1,30 @@
 <section class="container">
-    <?php require __DIR__ . '/../partials/pictures.tpl.php' ?>
+    <h1>Gallery</h1>
+    <div class="d-flex justify-content-between flex-wrap">
+
+    <div class="card border-info my-4" style="width: 20rem;">
+        <img src="<?= $assetsBaseUri ?>images/gallery/BTW.png" class="card-img-top" alt="Between Two Ferns">
+        <div class="card-body text-center">
+            <h4 class="card-title text-info">Between Two Ferns</h4>
+            <h5>Sample in :</h5>
+            <p>Yarn : Félicette - <a target="_blank" href="https://lechatquitricote-shop.com/">Le Chat Qui Tricote</a></p>
+            <p>75% merino - 25% nylon - 5% stellina</p>
+            <p>Colorway : Olive</p>
+            <p>Size : M</p>
+        </div>
+    </div>
+
+    <div class="card border-info my-4" style="width: 20rem;">
+        <img src="<?= $assetsBaseUri ?>images/gallery/VFV.png" class="card-img-top" alt="V For Vendetta">
+        <div class="card-body text-center">
+            <h4 class="card-title text-info">V for Vendetta</h4>
+            <h5>Sample in :</h5>
+            <p>Yarn : Figaro - <a target="_blank" href="https://lechatquitricote-shop.com/">Le Chat Qui Tricote</a></p>
+            <p>75% merino - 20% yak - 10% nylon</p>
+            <p>Colorway : Paon</p>
+            <p>Size : M</p>
+        </div>
+    </div>
+
+    </div>
 </section>
