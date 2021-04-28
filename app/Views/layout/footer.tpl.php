ecdda72849255995cb76043b46bc9bda51452619
namlee-dev <contact@namlee-dev.com>
2021-04-28 17:36:14 +0200

    remove cookies in footer

--- app/Views/layout/footer.tpl.php
+++ app/Views/layout/footer.tpl.php
@@ -20,7 +20,5 @@
 <!-- Bootstrap -->
 <script src="<?= $assetsBaseUri ?>js/jquery.slim.min.js"></script>
 <script src="<?= $assetsBaseUri ?>js/bootstrap.bundle.min.js"></script>
-<!-- Cookies -->
-<!-- <script type="text/javascript" charset="UTF-8" src="//cdn.cookie-script.com/s/d13546485991dc9e863c902fcc9c5bc9.js"></script> -->
 </body>
 </html>
